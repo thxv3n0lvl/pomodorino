26c5285f7354580c0796d7fa882ccb9f2e95811f
Alan Trevino <thxv3n0lvl@gmail.com>
2020-03-11 22:33:10 +0100

    Add more progress on time bar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let timerUI = document.querySelector('div.counter h1');
 let playerUI = document.querySelector('.player');
 let activityUI = document.querySelector('.activity');
+let barUI = document.querySelector('.bar');
 let intervalId = null;
 const playerPauseIcon = 'https://img.icons8.com/metro/26/000000/pause.png';
 const playerPlayIcon = 'https://img.icons8.com/android/24/000000/play.png';
@@ -10,8 +11,15 @@ const playerPlayIcon = 'https://img.icons8.com/android/24/000000/play.png';
 var initTimer = () => {
   playerUI.src = playerPauseIcon;
   intervalId = window.setInterval(() => {
-    timerUI.innerHTML == 0 ? endTimer() : timerUI.innerHTML --;
+    if(timerUI.innerHTML == 0) {
+      endTimer();
+    } else {
+      timerUI.innerHTML --;
+
+    }
+
   }, 1000); // 60000 Executed every minute
+  barUI.setAttribute('style', `animation: foobar ${timerUI.innerHTML * 1000}ms;`);
 }
 
 const endTimer = () => {
@@ -21,12 +29,14 @@ const endTimer = () => {
 }
 
 const startTimer = () => {
+
   if(playerUI.src.indexOf('play') > 0
       && !Number.isInteger(intervalId)) {
     initTimer();
   } else {
     endTimer();
   }
+
 }
 
 const setActivity = event => {
@@ -49,6 +59,7 @@ const switchActivity = () => {
     endTimer();
   }
 }
+
 
 playerUI.addEventListener('click', startTimer);
 activityUI.addEventListener('keydown', setActivity);
